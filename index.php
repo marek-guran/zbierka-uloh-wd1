26ce76bf67636a5d46d43ebbbd2d04659ac6112e
marek-guran <skemgi2@gmail.com>
2023-10-07 10:34:17 +0200

    update

--- index.php
+++ index.php
@@ -1,62 +1,68 @@
 <!DOCTYPE html>
 <html lang="sk">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zbierka úloh pre WD1</title>
     <link rel="stylesheet" href="/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand ml-3" href="#" style="margin-left: 50px;">Zbierka Úloh</a>
-    <ul class="navbar-nav mx-auto">
-        <li class="nav-item">
-            <a class="nav-link btn btn-outline-primary" href="index.php">Domov</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link btn btn-outline-primary" href="html_page.php">HTML</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link btn btn-outline-primary" href="css_page.php">CSS</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link btn btn-outline-primary" href="javascript_page.php">JavaScript</a>
-        </li>
-    </ul>
-    <button class="btn btn-outline-primary" id="loginButton" style="margin-right: 50px;">Login</button>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand ml-3" href="#" style="margin-left: 50px;">Zbierka Úloh</a>
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+                <a class="nav-link btn btn-outline-primary" href="index.php">Domov</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn btn-outline-primary" href="html_page.php">HTML</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn btn-outline-primary" href="css_page.php">CSS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn btn-outline-primary" href="javascript_page.php">JavaScript</a>
+            </li>
+        </ul>
+        <button class="btn btn-outline-primary" id="loginButton" style="margin-right: 50px;">Login</button>
+    </nav>
 
-<div class="container mt-4">
-    <div class="row">
-        <div class="col-md-4 mb-4">
-            <a href="html_page.php" class="card-link">
-                <div class="card">
-                    <img src="/img/html.jpg" class="card-img-top" alt="HTML Image">
-                </div>
-            </a>
-        </div>
-        <div class="col-md-4 mb-4">
-            <a href="css_page.php" class="card-link">
-                <div class="card">
-                    <img src="/img/css.webp" class="card-img-top" alt="CSS Image">
-                </div>
-            </a>
-        </div>
-        <div class="col-md-4 mb-4">
-            <a href="javascript_page.php" class="card-link">
-                <div class="card">
-                    <img src="/img/js.png" class="card-img-top" alt="JavaScript Image">
-                </div>
-            </a>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <a href="html_page.php" class="card-link">
+                    <div class="card">
+                        <img src="/img/html.jpg" class="card-img-top" alt="HTML Image">
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 mb-4">
+                <a href="css_page.php" class="card-link">
+                    <div class="card">
+                        <img src="/img/css.webp" class="card-img-top" alt="CSS Image">
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 mb-4">
+                <a href="javascript_page.php" class="card-link">
+                    <div class="card">
+                        <img src="/img/js.png" class="card-img-top" alt="JavaScript Image">
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
-</div>
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
