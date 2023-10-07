c98fc89fe6c5a7b731145a2c3c6b037bb3c41dc3
marek-guran <skemgi2@gmail.com>
2023-10-07 11:26:20 +0200

    malé zmeny

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand ml-3" href="#" style="margin-left: 50px;">Zbierka Úloh</a>
+    <a class="navbar-brand ml-3" href="#" style="margin-left: 50px;">Zbierka Úloh - WD1</a>
     <ul class="navbar-nav mx-auto">
         <li class="nav-item">
             <a class="nav-link btn btn-outline-primary" href="index.php">Domov</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link btn btn-outline-primary" href="html_page.php">HTML</a>
+            <a class="nav-link btn btn-outline-primary" href="/pages/html_page.php">HTML</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link btn btn-outline-primary" href="css_page.php">CSS</a>
+            <a class="nav-link btn btn-outline-primary" href="/pages/css_page.php">CSS</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link btn btn-outline-primary" href="javascript_page.php">JavaScript</a>
+            <a class="nav-link btn btn-outline-primary" href="/pages/js_page.php">JavaScript</a>
         </li>
     </ul>
     <button class="btn btn-outline-primary" id="loginButton" style="margin-right: 50px;">Login</button>
