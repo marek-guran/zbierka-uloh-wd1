70957a631eacc9f6dcb58a9be4a2b5345a9d6496
marek-guran <skemgi2@gmail.com>
2023-10-07 13:02:27 +0200

    malé zmeny

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,36 @@
+<link rel="stylesheet" href="/fonts/fontawesome-free-6.4.2-web/css/all.css">
 <footer>
+    <div class="content">
+        <ul class="socials">
+            <li class="social">
+                <a href="mailto:guran.marek1999@gmail.com" target="_blank" class="email">
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                </a>
+            </li>
+            <li class="social">
+                <a href="https://www.linkedin.com/in/marek-guran/" target="_blank" class="linkedin">
+                    <i class="fab fa-linkedin" aria-hidden="true"></i>
+                </a>
+            </li>
+            <li class="social">
+                <a href="https://github.com/marek-guran" target="_blank" class="github">
+                    <i class="fab fa-github" aria-hidden="true"></i>
+                </a>
+            </li>
+            <li class="social">
+                <a rel="me" href="https://mastodon.social/@marek_guran" target="_blank" class="mastodon">
+                    <i class="fab fa-mastodon" aria-hidden="true"></i>
+                </a>
+            </li>
+        </ul>
+        <div class="clear"></div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <p>&copy; <?php echo date("Y"); ?> Marek Guráň</p>
+                <p>&copy;
+                    <?php echo date("Y"); ?> Marek Guráň
+                </p>
             </div>
         </div>
     </div>
