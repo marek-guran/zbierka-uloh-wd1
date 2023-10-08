48ad38062fa89a3735ce2d323bc917ccbd10b844
marek-guran <skemgi2@gmail.com>
2023-10-08 10:50:22 +0200

    Podpora malých obrazoviek a nové farby

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
         <a class="navbar-brand ml-3" href="../index.php">Zbierka Úloh - WD1</a>
-        <ul class="navbar-nav mx-auto" style="margin-right: 0px!important;">
-            <li class="nav-item">
-                <a class="nav-link btn btn-outline-primary" href="../index.php">Domov</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link btn btn-outline-primary" href="../pages/html_page.php">HTML</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link btn btn-outline-primary" href="../pages/css_page.php">CSS</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link btn btn-outline-primary" href="../pages/js_page.php">JavaScript</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link btn btn-outline-primary" href="../pages/priklady.php">Príklady</a>
-            </li>
-        </ul>
+
+        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto" style="margin-right: 0px!important;">
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-primary" href="../index.php">Domov</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-primary" href="../pages/html_page.php">HTML</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-primary" href="../pages/css_page.php">CSS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-primary" href="../pages/js_page.php">JavaScript</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-primary" href="../pages/priklady.php">Príklady</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
