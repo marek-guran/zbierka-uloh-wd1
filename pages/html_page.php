ffea4e616f46eaec14e5cf9edec1240fce2bec38
marek-guran <skemgi2@gmail.com>
2023-10-10 16:42:54 +0200

    Nalinkované css a scripty sú teraz lokálne

--- pages/html_page.php
+++ pages/html_page.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HTML</title>
     <link rel="stylesheet" href="/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/bootstrap-5.3.2.css">
     <link rel="icon" type="image/x-icon" href="../favicon.ico">
 </head>
 
@@ -27,11 +26,9 @@
 
     <script src="../js/show-code.js"></script>
     <script src="../js/sidebar-item-active.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <script src="../js/jquery-3.5.1.js"></script>
+    <script src="../js/popperjs_core-2.5.3.js"></script>
+    <script src="../js/bootstrap-5.3.2.js"></script>
     <!-- Sidebar tlačidlo musí byť vždy ako posledné!!! -->
     <script src="../js/sidebar-toggle.js"></script>
 </body>
