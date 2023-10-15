189360a10dca7495d2ddd4c30112d4c195a47d9e
marek-guran <skemgi2@gmail.com>
2023-10-15 18:02:33 +0200

    Opravený sidebar - aktívny item Teraz by už malo ukazovať správny aktívny item ak má stránka viac príkladov

--- js/sidebar-item-active.js
+++ js/sidebar-item-active.js
@@ -2,8 +2,13 @@ const menuItems = document.querySelectorAll('.sidebar-container .nav-link');
 
 menuItems[0].classList.add('active');
 
-menuItems.forEach(item => {
+menuItems.forEach((item, index) => {
     if (item.href === window.location.href) {
+        menuItems.forEach(item => {
+            item.classList.remove('active');
+        });
+        item.classList.add('active');
+    } else if (window.location.href.includes('css-animacie2') && index === 8) {
         menuItems.forEach(item => {
             item.classList.remove('active');
         });
