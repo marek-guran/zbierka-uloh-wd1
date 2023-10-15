0942e4798decf79a92536c04e5fd9741de42712d
marek-guran <skemgi2@gmail.com>
2023-10-15 16:20:15 +0200

    čo by mohlo byť v js stránke

--- includes/js-menu.php
+++ includes/js-menu.php
@@ -5,6 +5,42 @@
             <li class="nav-item">
                 <a class="nav-link" href="?pr=js-pr1">Test</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">API</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Grafy</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Navigačné menu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Alerting</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Prepínanie CSS štýlu</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Vyskakovacie okná</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Kalkulačka</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Hodiny</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Drag and Drop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Galéria obrázkov</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Tabuľky</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pr=js-pr1">Vyhľadávanie</a>
+            </li>
         </ul>
     </div>
 
