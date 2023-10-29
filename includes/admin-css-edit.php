a693d0b3fa3cb0f598e7a549aed1afb257562c65
marek-guran <skemgi2@gmail.com>
2023-10-29 13:03:59 +0100

    JS a CSS príklady sa teraz lepšie upravujú

--- includes/admin-css-edit.php
+++ includes/admin-css-edit.php
@@ -9,8 +9,44 @@
                 <div class="modal-body">
                     <div class="mb-3">
                         <label for="kategoria" class="form-label">Kategória</label>
-                        <input type="text" class="form-control" id="kategoria" name="kategoriaEdit"
-                            value="<?php echo $kategoria; ?>" required>
+                        <select class="form-select" id="kategoria-select" name="kategoriaEdit" required>
+                            <option value="new">Pridať novú kategóriu</option>
+                            <?php
+                            $hostEdit = 'zbierka-db';
+                            $portEdit = 3306;
+                            $usernameEdit = 'admin';
+                            $passwordEdit = getenv('TEACHER_PASSWORD');
+                            $databaseEdit = 'zbierka';
+
+                            $connectionEdit = new mysqli($hostEdit, $usernameEdit, $passwordEdit, $databaseEdit, $portEdit);
+
+                            if ($connectionEdit->connect_error) {
+                                die('Connection failed: ' . $connectionEdit->connect_error);
+                            }
+
+                            // Get the existing categories
+                            $stmtCategories = $connectionEdit->prepare("SELECT DISTINCT kategoria FROM CSS");
+                            $stmtCategories->execute();
+                            $resultCategories = $stmtCategories->get_result();
+
+                            // Loop through the categories and add them to the dropdown
+                            while ($rowCategories = $resultCategories->fetch_assoc()) {
+                                $category = $rowCategories['kategoria'];
+                                if ($category == $kategoria) {
+                                    echo "<option value=\"$category\" selected>$category</option>";
+                                } else {
+                                    echo "<option value=\"$category\">$category</option>";
+                                }
+                            }
+
+                            $stmtCategories->close();
+                            $connectionEdit->close();
+                            ?>
+                        </select>
+                    </div>
+                    <div class="mb-3" id="kategoria-form" style="display: none;">
+                        <label for="kategoria" class="form-label">Nová kategória</label>
+                        <input type="text" class="form-control" id="kategoria" name="kategoria">
                     </div>
                     <div class="mb-3">
                         <label for="nazov" class="form-label">Názov</label>
@@ -39,10 +75,29 @@
     </div>
 </div>
 
+<script>
+    const kategoriaSelect = document.getElementById('kategoria-select');
+    const kategoriaInput = document.getElementById('kategoria');
+    const kategoriaForm = document.getElementById('kategoria-form');
+    kategoriaSelect.addEventListener('change', () => {
+        if (kategoriaSelect.value === 'new') {
+            kategoriaInput.required = true;
+            kategoriaForm.style.display = 'block';
+        } else {
+            kategoriaInput.required = false;
+            kategoriaForm.style.display = 'none';
+            document.getElementById('kategoria').value = kategoriaSelect.value;
+        }
+    });
+</script>
+
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $idEdit = $id;
     $kategoriaEdit = $_POST['kategoriaEdit'];
+    if ($kategoriaEdit === 'new') {
+        $kategoriaEdit = $_POST['kategoria'];
+    }
     $nazovEdit = $_POST['nazovEdit'];
     $zadanieEdit = $_POST['zadanieEdit'];
     $visibleVysledokEdit = isset($_POST['visible-vysledokEdit']) && !empty($_POST['visible-vysledokEdit']);
